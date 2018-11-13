25773d5c2c4dcaee4e36d1cd0b3346adce703f3d
国投 <zhangjinliang@wypuhui.com>
2018-11-13 17:35:25 +0800

    1

--- RxSwiftByMvvm/Root/FirstPage/FirstPageViewController.swift
+++ RxSwiftByMvvm/Root/FirstPage/FirstPageViewController.swift
@@ -10,6 +10,7 @@ import Foundation
 import UIKit
 import RxSwift
 import RxCocoa
+import RxDataSources
 
 
 class FirstPageViewController: UIViewController {
@@ -18,7 +19,6 @@ class FirstPageViewController: UIViewController {
 
     private let disBg = DisposeBag()
 
-    static let cellid = "FirstPageTableViewCell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,43 +29,79 @@ class FirstPageViewController: UIViewController {
         tableView.tableFooterView = UIView()
 //        tableView.dataSource = self
 
-        let item = Observable.just([
-            "文本输入框的用法",
-            "开关按钮的用法",
-            "进度条的用法",
-            "文本标签的用法",
-            ])
+        let sections = Observable.just([
+            ListDataBean.init(model: "123", items: [
+                "12",
+                "233",
+                "434"
+                ]),
+            ListDataBean.init(model: "123", items: [
+                "12",
+                "233",
+                "434"
+                ]),
+            ListDataBean.init(model: "123", items: [
+                "12",
+                "233",
+                "434"
+                ])
+        ])
 
-
-        //设置单元格数据（其实就是对 cellForRowAt 的封装）
-        let _ = item.bind(to: tableView.rx.items(cellIdentifier: "FirstPageTableViewCell"))({ index,info,datacell in
-            if let cell = datacell as? FirstPageTableViewCell {
+        let dataSource = RxTableViewSectionedReloadDataSource<ListDataBean>(
+            configureCell: { dataSource,tableview,indexPath,info in
+                let cell = tableview.dequeueReusableCell(withIdentifier: "FirstPageTableViewCell", for: indexPath) as! FirstPageTableViewCell
                 cell.textlabel.text = info
-            }
-        })
+                return cell
+        },
+            //设置分区头标题
+            titleForHeaderInSection: { ds, index in
+                return ds.sectionModels[index].title
+        }
+        )
 
-        tableView.rx.itemSelected.subscribe(onNext: { [weak self] indexPath in
-            self?.tableView.deselectRow(at: indexPath, animated: true)
-            print("选中项的indexPath为：\(indexPath)")
-        }).disposed(by: disBg)
+        sections.bind(to: tableView.rx.items(dataSource: dataSource))
 
-         
+//        let item = Observable.just([
+//            "文本输入框的用法",
+//            "开关按钮的用法",
+//            "进度条的用法",
+//            "文本标签的用法",
+//            ])
+//        //设置单元格数据（其实就是对 cellForRowAt 的封装）
+//        let _ = item.bind(to: tableView.rx.items(cellIdentifier: "FirstPageTableViewCell"))({ index,info,datacell in
+//            if let cell = datacell as? FirstPageTableViewCell {
+//                cell.textlabel.text = info
+//            }
+//        })
+//
+//        tableView.rx.itemSelected.subscribe(onNext: { [weak self] indexPath in
+//            self?.tableView.deselectRow(at: indexPath, animated: true)
+//            print("选中项的indexPath为：\(indexPath)")
+//        }).disposed(by: disBg)
+
+
 
     }
 
 
 }
 
-extension FirstPageViewController: UITableViewDelegate,UITableViewDataSource {
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+class ListDataBean: SectionModelType {
+
+    typealias Item = String
+
+    var items: [String] = []
+
+    var title: String = "你好啊"
+    var desc: String = "呵呵"
+
+    required init(original: ListDataBean, items: [String]) {
+
     }
 
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: FirstPageViewController.cellid) as! FirstPageTableViewCell
-        cell.textlabel.text = "12"
-        return cell
-    }
+
+
+
 
 }
